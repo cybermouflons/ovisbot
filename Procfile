16d6fa9ec5696f710adb7e056c3594ed5939f6b2
apogiatzis <apoyiatzis@outlook.com>
2019-05-24 02:39:08 +0300

    test drive

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python core.py
--- core.py
+++ core.py
@@ -1,6 +1,23 @@
 import discord
+import os
 
 from discord.ext.commands import Bot
 from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv("DISCORD_BOT_TOKEN")
 
 client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("I'm in")
+    print(client.user)
+
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+        await message.channel.send(message.content[::-1])
+
+client.run(token)
