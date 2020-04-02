41f02d516e3e1f458c559d29199440c6117f8979
apogiatzis <apoyiatzis@outlook.com>
2020-04-02 11:55:38 +0100

    fix(ctf): hotfix status when no challenges

--- ovisbot/extensions/utils.py
+++ ovisbot/extensions/utils.py
@@ -18,20 +18,20 @@ class Utils(commands.Cog):
 
     @utils.command(aliases=["stol"])
     async def str2long(self, ctx, params):
-        await ctx.send(bytes_to_long(params.encode("utf-8")))
+        await ctx.send("`{0}`".format(bytes_to_long(params.encode("utf-8"))))
 
     @utils.command(aliases=["ltos"])
     async def long2str(self, ctx, params):
-        await ctx.send(long_to_bytes(params))
+        await ctx.send("`{0}`".format(long_to_bytes(params)))
 
     @utils.command()
     async def str2hex(self, ctx, *params):
         joined_params = " ".join(params)
-        await ctx.send(joined_params.encode("latin-1").hex())
+        await ctx.send("`{0}`".format(joined_params.encode("latin-1").hex()))
 
     @utils.command()
     async def hex2str(self, ctx, param):
-        await ctx.send(bytearray.fromhex(param).decode())
+        await ctx.send("`{0}`".format(bytearray.fromhex(param).decode()))
 
 
 def setup(bot):
