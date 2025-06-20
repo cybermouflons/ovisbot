6a4d8c3a891eecf63a5553fbb41b9712c2d3e77c
Christos Falas <raspberrymaker@gmail.com>
2025-06-20 07:22:36 +0100

    Migrate to slash commands

--- ovisbot/exceptions.py
+++ ovisbot/exceptions.py
@@ -1,64 +1,60 @@
-class ChallengeDoesNotExistException(Exception):
-    pass
+class OvisBotException(Exception):
+    message: str = ""
 
 
-class ChallengeExistsException(Exception):
-    pass
+class ChallengeDoesNotExistException(OvisBotException):
+    message = "Παρέα μου... εν κουτσιάς... Εν έσιει έτσι challenge!"
 
 
-class ChallengeInvalidCategory(Exception):
-    pass
+class ChallengeExistsException(OvisBotException):
+    message = "Να μου γελάσεις ρε κοπελλούι; This challenge already exists!"
 
-class ChallengeInvalidDifficulty(Exception):
-    pass
 
-class ChallengeAlreadySolvedException(Exception):
-    def __init__(self, solved_by, *args, **kwargs):
+class ChallengeAlreadySolvedException(OvisBotException):
+    def __init__(self, solved_by: list[str]):
         self.solved_by = solved_by
+        self.message = f"Άρκησες! This challenge has already been solved by {', '.join(self.solved_by)}!"
 
 
-class ChallengeNotSolvedException(Exception):
-    pass
+class ChallengeNotSolvedException(OvisBotException):
+    message = "Ρε κουμπάρε.. αφού ένεν λυμένη η άσκηση."
 
 
-class UserAlreadyInChallengeChannelException(Exception):
-    pass
+class FewParametersException(OvisBotException):
+    message = "Ρε παρέα μου κάτι σου λύφκεται. You need to provide more parameters for this command."
 
 
-class FewParametersException(Exception):
-    pass
+class NotInChallengeChannelException(OvisBotException):
+    message = "Ρε πελλοβρεμένε! For this command you have to be in a ctf challenge channel created by `!ctf addchallenge`."
 
 
-class NotInChallengeChannelException(Exception):
-    pass
+class NotInCTFChannelException(OvisBotException):
+    message = "Ρε πελλοβρεμένε! For this command you have to be in a ctf channel created by `!ctf create`."
 
 
-class CTFAlreadyExistsException(Exception):
-    pass
+class CTFAlreadyExistsException(OvisBotException):
+    message = "Ρε κουμπάρε! This CTF name already exists! Pick another one"
 
 
-class CTFSharedCredentialsNotSet(Exception):
-    pass
+class CTFSharedCredentialsNotSet(OvisBotException):
+    message = "This CTF has no shared credentials set!"
 
 
-class CTFAlreadyFinishedException(Exception):
-    pass
+class CTFAlreadyFinishedException(OvisBotException):
+    message = "This CTF has already finished!"
 
 
+# TODO: remove
 class CtfimeNameDoesNotMatch(Exception):
     pass
 
 
-class DateMisconfiguredException(Exception):
-    pass
+class DateMisconfiguredException(OvisBotException):
+    message = "Έκαμες τα σαλάτα με τις ημερομηνίες παλε... Πρέπει Start date > End Date"
 
 
-class MissingStartDateException(Exception):
-    pass
-
-
-class MissingEndDateException(Exception):
-    pass
+class MissingStartDateException(OvisBotException):
+    message = "Φιλούδιν... πρέπει να βάλεις ενα start date (!ctf startdate ...) πρώτα!"
 
 
 class CryptoHackApiException(Exception):
